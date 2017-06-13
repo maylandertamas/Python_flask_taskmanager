a0f00e81d3ad8a7cf1f5678848f9b02414169b61
Hadroon <gabor.muranyi@gmail.com>
2017-06-13 10:31:32 +0200

    getJSON and print to the main page

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,9 @@
-alert("Hello Gabor")
+
+$.getJSON("/static/database/sample_data.json", function(json){
+    // console.log(json.boards[1].title);
+    var jsonLength = json.boards.length;
+    for (i = 0; i < jsonLength; i++) {
+        console.log(json.boards[i].title);
+        $("#boards").append("<div>" + json.boards[i].title + "</div>");    
+    }
+});
