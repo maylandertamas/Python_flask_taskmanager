7d14ecdb29db16c7d69eecf3925610112ce3fc18
Hadroon <gabor.muranyi@gmail.com>
2017-06-13 21:37:19 +0200

    Add board with id. Click on board show board's card's.

--- static/js/main.js
+++ static/js/main.js
@@ -1,41 +1,58 @@
 function printBoards(dataObject) {
+    $('.boards').empty();
     for (var i = 0; i < Object.keys(dataObject.boards).length; i++) {
-        $("#boards-container").append("<div class='col-md-3 board'>" + dataObject.boards[i].title + "</div>");
+    $(".boards").append("<div class='col-md-3 board' data-board-id='" + i + "'>" + dataObject.boards[i].title + "</div>");
     }
-    $("#boards-container").append("<div class='col-md-3 board' id='new-board'>" + "New" + "</div>");
-    $("#boards-container").append("<div class='board'><input id='input-field' type='text'><span id='add-button'> +</span></div>");
-    $('#input-field').hide();
 }
 
-function createNewBoard(dataObject) {
-    $("#new-board").click(function() {
-        $(this).hide();
-        $('#input-field').show();
-        $('#add-button').click(function(){
+function main() {
+
+    // Read JSON to get initial data.
+    setupJson();
+
+    // Create an object from local storage string.
+    var dataObject = readFromJson();
+
+    // Print boards.
+    printBoards(dataObject);
+
+    // Add create new board field.
+    $("#new-board").append("<div class='board-input'><input id='input-field' type='text' placeholder='New board'><span id='add-button'> +</span></div>");
+
+    // Show the clicked board cards
+    $(document).on("click" , ".board", function(){
+
+        // Hide boards container and show cards container
+        $("#boards-container").css({"display": "none"});
+        $("#cards-container").css({"display": "block"});
+
+        // Get board id from html data.
+        var boardId = $(this).data("board-id");
+
+        // Append cards container with cards
+        for (var i = 0; i < Object.keys(dataObject.boards[boardId].cards).length; i++) {
+        $("#cards-container").append("<div class='col-md-3 card'>" + dataObject.boards[boardId].cards[i].title + "</div>");
+        }
+    
+    });
+
+    $('#add-button').click(function(){
+
+            // Get the new board title from impu.
             var newBoardTitle = $(this).prev().val();
+
             var newObject =  {
-                "id": 3,
+                "id": Object.keys(dataObject.boards).length,
                 "title": newBoardTitle,
                 "state": "active",
                 "cards": []
             }
             dataObject.boards.push(newObject);
-            $('#boards-container').empty();
+            // alert(dataObject.boards[boardId].cards[i].id);
+            // alert(Object.keys(dataObject.boards))
             return printBoards(dataObject);
-        $(this).show();
         });
-    });
     
 }
-
-function main() {
-    setupJson();
-    var dataObject = readFromJson();
-    printBoards(dataObject);
-    createNewBoard(dataObject);
-    
-}
-
-
 
 $(document).ready(main);
