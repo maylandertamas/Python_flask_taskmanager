53acc295a8ffcf69126192de25b257b946db56c2
Peter Korossy <meanwhile@vipmail.hu>
2017-06-13 13:49:25 +0200

    Added listBoards func

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,18 @@
-setupJson();
-var jsonObject =  readFromJson()
-console.log(jsonObject.boards[0].title);
+function printBoards(dataObject) {
+    for (var i = 0; i < Object.keys(dataObject.boards).length; i++) {
+        console.log(i);
+        $("#boards-container").append("<div class='col-md-3 board'>" + dataObject.boards[i].title + "</div>");
+    }
+}
+
+
+function main() {
+    setupJson();
+    var dataObject = readFromJson();
+    
+    printBoards(dataObject);
+}
+
+
+
+$(document).ready(main);
