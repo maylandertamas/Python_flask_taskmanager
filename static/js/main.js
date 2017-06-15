19d8f80753f9b3c7bc0261fea30788cbf0a399e3
Hadroon <gabor.muranyi@gmail.com>
2017-06-15 09:06:53 +0200

    Refactor cards page and clear code

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,6 @@
 function printBoards(dataObject) {
+
+    // For to get boards.
     for (var i = 0; i < Object.keys(dataObject.boards).length; i++) {
     $("#boards-container").append("<div class='board' data-board-id='" + i + "'>" 
     + dataObject.boards[i].title + "</div>");
@@ -7,33 +9,37 @@ function printBoards(dataObject) {
 
 function showCardPage(dataObject) {
     $(document).on("click" , ".board", function(){
-        // Hide boards container and show cards container
+
+        // Hide boards container and show cards container.
         $("#boards-container").css({"display": "none"});
         $("#boards-head").css({"display": "none"});
         $("#cards-head").css({"display": "block"});
         $('#cards-container').css({"display": "block"})
-
         $("#cards-head").append("<button type='button' class='btn' id='back-button'>BACK</button>");
+
         // Get board id from html data.
         boardId = $(this).data("board-id");
         var boardTitle = dataObject.boards[boardId].title;
         $('#cards-head').append('<h1 id="board-title">' + boardTitle + '</h1>');
-        $("#cards-head").append("<div class='card-input'><input id='card-input-field' type='text' placeholder='New card'><span id='add-card-button' data-board-id='" + boardId + "'> +</span></div>");
-        // Append cards container with cards
+        $("#cards-head").append("<div class='card-input'><input id='card-input-field' type='text' placeholder='Create new card'><span id='add-card-button' data-board-id='" + boardId + "'> +</span></div>");
+
+        // Append cards to the proper container
         for (var i = 0; i < Object.keys(dataObject.boards[boardId].cards).length; i++) {
-            $("#new").append("<div class='card actual-cards'>" + dataObject.boards[boardId].cards[i].title + "</div>");
-        }
-        /*
-        for (var i = 0; i < Object.keys(dataObject.boards[boardId].cards).length; i++) {
-        $("#cards-container").append("<div class='col-md-3 card'>" + dataObject.boards[boardId].cards[i].title + "</div>");
-    }
-    */
-    
+            console.log(dataObject.boards[boardId].cards.status)
+            switch (dataObject.boards[boardId].cards[i].status) {
+                case "in progress": $("#in-progress").append("<div class='card actual-cards'>" + dataObject.boards[boardId].cards[i].title + "</div>");
+                    break;
+                case "review": $("#review").append("<div class='card actual-cards'>" + dataObject.boards[boardId].cards[i].title + "</div>");
+                    break;
+                case "done": $("#done").append("<div class='card actual-cards'>" + dataObject.boards[boardId].cards[i].title + "</div>");
+                    break;
+                default: $("#new").append("<div class='card actual-cards'>" + dataObject.boards[boardId].cards[i].title + "</div>");
+            }
+        }   
     });
 }
 
 function addBoard(dataObject) {
-     // $("#new-board").append("<div class='board-input'><input id='input-field' type='text' placeholder='New board'><span id='add-board-button'> +</span></div>");
      $('#add-board-button').click(function(){
 
             // Get the new board title from impu.
@@ -66,18 +72,7 @@ function addNewCard(dataObject) {
          dataObject.boards[boardId].cards.push(newObject);
          console.log(newObject);
          $("#new").append("<div class='card actual-cards'>" + newObject.title + "</div>");
-         // printCards(dataObject, boardId);
         });
-}
-
-function printCards(dataObject, boardId) {
-    // $("#prog-container").css({"display": "block"});
-    $(".actual-cards").remove();
-    // $("#cards-container").append("<button type='button' class='btn' id='back-button'>BACK</button>");
-    // $("#cards-container").append("<div class='card-input'><input id='card-input-field' type='text' placeholder='New card'><span id='add-card-button'> +</span></div>")
-    for (var i = 0; i < Object.keys(dataObject.boards[boardId].cards).length; i++) {
-            $("#new").append("<div class='card actual-cards'>" + dataObject.boards[boardId].cards[i].title + "</div>");
-    }
 }
 
 
@@ -91,8 +86,6 @@ function backToBoardPage(dataObject) {
         $(".actual-cards").remove();
         $("#board-title").remove();
         $("#input-field").val("");
-        // $('#header-container').css({"display": "none"})
-
     });
 }
 
@@ -104,16 +97,20 @@ function main() {
     // Create an object from local storage string.
     var dataObject = readFromJson();
     var boardId;
+
     // Print boards.
     printBoards(dataObject);
 
-    // Add create new board field.
-    
+    // Add create new board field.    
     addBoard(dataObject);
 
     // Show the clicked board cards
     showCardPage(dataObject);
+
+    // Add new card to board.
     addNewCard(dataObject);
+
+    // Back to the boards page.
     backToBoardPage(dataObject);
 }
 
